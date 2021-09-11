6b13c71beb18c24efff8d51c4d57395c7692e756
= <venkat.surakka@outlook.com>
2021-09-11 21:59:01 +1200

    changed contact-us component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  styleUrls: ['../styles.css'],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -2,11 +2,20 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact-us',
+  styleUrls: ['./contact-us.component.css'],
   templateUrl: './contact-us.component.html'
 })
-export class ContactUsComponent implements OnInit {
 
-  constructor() { }
+export class ContactUsComponent implements OnInit {
+  ownerName: string;
+  emailOne: string;
+  emailTwo: string;
+
+  constructor() {
+    this.ownerName = 'Mr. Ravi Kumar Murugani';
+    this.emailOne = 'ravialstom@gmail.com';
+    this.emailTwo = 'alstomhealthcare@gmail.com';
+   }
 
   ngOnInit(): void {
   }
